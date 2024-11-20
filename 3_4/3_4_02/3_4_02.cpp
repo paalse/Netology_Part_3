6ffac411f67133812bd9c501ec0836abaab175a8
Alex Pavlov <paalse@list.ru>
2024-11-20 18:06:04 +0400

    Home work of lesson 4.02

---
+++ 3_4/3_4_02/3_4_02.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+
+class Address {
+	private:
+		std::string sity;
+		std::string street;
+		int home;
+		int flat;
+
+	public:
+		Address(std::string inSity, std::string inStreet, int inHome, int inFlat)
+			:sity(inSity), street(inStreet), home(inHome), flat(inFlat)
+		{};
+		
+		std::string printAddress() {
+			return sity + ", " + street + ", " + std::to_string(home) + ", " + std::to_string(flat);
+		}
+
+		std::string getSity() {
+			return sity;
+		}
+};
+
+// Сортировка массива адресов по городу
+void sort(Address** addrArray, int size){
+
+	Address* tmp;
+
+	for (int i=0; i<size;i++)
+	{ 
+		for (int j = 0; j < size - 1; j++)
+		{
+	
+			if (addrArray[j]->getSity() > addrArray[j+1]->getSity()) {
+				tmp = addrArray[j];
+				addrArray[j] = addrArray[j + 1];
+				addrArray[j + 1] = tmp;
+			}
+			
+		}
+	}
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	std::ifstream fin("in.txt");
+	std::ofstream fout("out.txt");
+	std::string str = "";
+	int cnt = 0;
+	std::string sity;
+	std::string street;
+	int home;
+	int flat;
+
+	if (fin.is_open()) {
+		fin >> str;
+		cnt = stoi(str);
+		Address** addrArray = new Address*[cnt];
+
+		// Получение данных из файла
+		for (int i=0; i < cnt; i++) {
+			fin >> sity;
+			fin >> street;
+			fin >> home;
+			fin >> flat;
+			Address* addr = new Address(sity, street, home, flat);
+			addrArray[i] = addr;
+		}
+
+		sort(addrArray, cnt); // Сортировка массива адресов
+		
+		// Запись в файл
+		if (fout.is_open()) {
+			for (int i = 0; i < cnt; i++) {
+				std::cout << addrArray[i]->printAddress() << std::endl;
+				fout << addrArray[i]->printAddress() << "\n";
+			}
+		}
+		else {
+			std::cout << "Ошибка открытия файла out.txt";
+		}
+		
+		// Освобождение памяти
+		for (int i=0; i < cnt; i++) {
+			delete addrArray[i];
+		}
+		delete[] addrArray;
+	}
+	else {
+		std::cout << "Ошибка открытия файла in.txt";
+	}
+
+	fin.close();
+	fout.close();
+
+	return EXIT_SUCCESS;
+}
