636096469848d350cc24e3225645be0f8f36e73c
Alex Pavlov <paalse@list.ru>
2024-11-27 20:20:33 +0400

    Home work of lesson 5, exercise 01

--- 3_5/3_5_01/3_5_01.cpp
+++ 3_5/3_5_01/3_5_01.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
 #include <Windows.h>
 
-class Figure {
-private:
-	int sides_count;
-public:
-	Figure():sides_count(0)	{};
-};
+namespace my {
+	class Figure {
+	private:
+		int sides_count;
+	public:
+		Figure() : sides_count(0) {};
 
-class Triangle : public Figure {
-private:
-	int sides_count;
-public:
-	Triangle(int sidesCnt) : sides_count(sidesCnt) {};
-};
+		Figure(int sidesCnt) : sides_count(sidesCnt) {};
 
-class Rectangle : public Figure {
-private:
-	int sides_count;
-public:
-	Rectangle(int sidesCnt) : sides_count(sidesCnt) {};
-};
+		int get_sides_count() {
+			return sides_count;
+		}
+	};
 
+	class Triangle : public Figure {
+	public:
+		Triangle() : Figure(3) {};
+
+	};
+
+	class Rectangle : public Figure {
+	public:
+		Rectangle() : Figure(4) {};
+	};
+
+}
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	std::cout << "Количество сторон:" << std::endl;
 
+	my::Figure figFig;
+	std::cout << "Фигура: " << figFig.get_sides_count() << std::endl;
+
+	my::Triangle figTriangle;
+	std::cout << "Треугольник: " << figTriangle.get_sides_count() << std::endl;
+
+	my::Rectangle figRectangle;
+	std::cout << "Прямоугольник: " << figRectangle.get_sides_count() << std::endl;
+	
 	return 0;
 }
