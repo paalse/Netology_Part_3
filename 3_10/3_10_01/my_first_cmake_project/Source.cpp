61453b6f3214f562f15f01fabe960259ee51d798
Alex Pavlov <paalse@list.ru>
2024-12-19 16:17:08 +0400

    Home work of lesson 10, excercise 01.

---
+++ 3_10/3_10_01/my_first_cmake_project/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string name;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!\n";
+
+	return 0;
+}
