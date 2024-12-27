ec59bdb86ac8fe565326f0ee00cbc7d2715b4d1a
Alex Pavlov <paalse@list.ru>
2024-12-27 16:57:33 +0400

    Home work of lesson 11, exercise 03, change Leaver.h

--- 3_11/3_11_03/Leaver.h
+++ 3_11/3_11_03/Leaver.h
@@ -1,12 +1,6 @@
 #pragma once
-#ifdef LEAVERLIBRARY_EXPORTS
-#define LEAVERLIBRARY __declspec(dllexport)
-#else
-#define LEAVERLIBRARY __declspec(dllimport)
-#endif
-
 
 class Leaver {
 public:
-	std::string LEAVERLIBRARY leave(std::string name);
+	std::string leave(std::string name);
 };
